f19e43fa385e28d85204334158a9389277edb16e
Vyacheslav L. <Dwek18@mail.ru>
2025-04-15 00:07:59 +0300

    1.14 String

---
+++ src/main/java/ru/condition/Greeting.java
@@ -0,0 +1,11 @@
+package ru.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        idea = idea + "But I am a newbie.";
+        int year = 2025;
+        idea += year;
+        System.out.println(idea);
+    }
+}
