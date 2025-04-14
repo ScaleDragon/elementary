c2248ed56479b01672c86ad7980a6b46c7d270cc
Vyacheslav L. <Dwek18@mail.ru>
2025-04-14 23:37:05 +0300

    1.12 Стороны прямоугольника

---
+++ src/main/java/ru/condition/SqArea.java
@@ -0,0 +1,18 @@
+package ru.condition;
+
+public class SqArea {
+    public static double square(double p, double k) {
+        double height = p / (2 * (k + 1));
+        double length = height * k;
+        double rsl = length * height;
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        double result1 = square(4, 1);
+        System.out.println(" p = 4, k = 1, s = 1, real = " + result1);
+
+        result1 = square(6, 2);
+        System.out.println(" p = 4, k = 1, s = 1, real = " + result1);
+    }
+}
