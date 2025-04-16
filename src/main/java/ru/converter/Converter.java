54a90398370e3c1e77b7133d4f4c8c3f292fac89
Vyacheslav L. <Dwek18@mail.ru>
2025-04-16 14:13:29 +0300

    2. Что такое тестирование

--- src/main/java/ru/converter/Converter.java
+++ src/main/java/ru/converter/Converter.java
@@ -12,6 +12,16 @@ public class Converter {
     }
 
     public static void main(String[] args) {
+        float in = 140F;
+        float expectedEuro = 1.4F;
+        float expectedDollar = 1.555555555555556F;
+        float outEuro = Converter.rubleToEuro(in);
+        float outDollar = Converter.rubleToDollar(in);
+        boolean passedEuro = expectedEuro == outEuro;
+        boolean passedDollar = expectedDollar == outDollar;
+        System.out.println("140 rubles are 1.4 euro Test result : " + passedEuro);
+        System.out.println("140 rubles are 1.555555555555556 dollar Test result : " + passedDollar);
+
         float euro = Converter.rubleToEuro(140);
         System.out.println("140 rubles are " + euro + " euro.");
         float dollar = rubleToDollar(140);
