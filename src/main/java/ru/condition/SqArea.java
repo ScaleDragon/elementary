07f111ebd75ac2f07698404e5b25704b74f30697
Vyacheslav L. <Dwek18@mail.ru>
2025-04-16 19:47:17 +0300

    1.12 Стороны прямоугольника

--- src/main/java/ru/condition/SqArea.java
+++ src/main/java/ru/condition/SqArea.java
@@ -13,6 +13,6 @@ public class SqArea {
         System.out.println(" p = 4, k = 1, s = 1, real = " + result1);
 
         result1 = square(6, 2);
-        System.out.println(" p = 6, k = 2, s = 1, real = " + result1);
+        System.out.println(" p = 6, k = 2, s = 2, real = " + result1);
     }
 }
