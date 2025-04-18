74a32ce3f4ba7790358cce4e51bcf4093b06c54c
Vyacheslav L. <Dwek18@mail.ru>
2025-04-19 00:31:28 +0300

    3.14. Match.abs

--- src/main/java/ru/condition/ChessBoard.java
+++ src/main/java/ru/condition/ChessBoard.java
@@ -1,12 +1,14 @@
 package ru.condition;
 
+import static java.lang.Math.abs;
+
 public class ChessBoard {
     public static int way(int x1, int y1, int x2, int y2) {
         int rsl = 0;
         if (isValid(x1) && isValid(x2)
                 && isValid(y1) && isValid(y2)) {
-            if (y2 == y1 - (x2 - x1) || y2 == y1 + (x2 - x1)) {
-                rsl = Math.abs(x2 - x1);
+            if (abs(x2 - x1) == abs(y2 - y1)) {
+                rsl = abs(x2 - x1);
             }
         }
         return rsl;
