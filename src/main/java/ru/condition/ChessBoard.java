4c9990dbb6c4666fb524a4611e00fe26c6beef2b
Vyacheslav L. <Dwek18@mail.ru>
2025-04-19 00:23:40 +0300

    3.14. Match.abs

--- src/main/java/ru/condition/ChessBoard.java
+++ src/main/java/ru/condition/ChessBoard.java
@@ -5,7 +5,7 @@ public class ChessBoard {
         int rsl = 0;
         if (isValid(x1) && isValid(x2)
                 && isValid(y1) && isValid(y2)) {
-            if (x1 != x2 && y1 != y2 && y2 == y1 - (x2 - x1) || y2 == y1 + (x2 - x1)) {
+            if (y2 == y1 - (x2 - x1) || y2 == y1 + (x2 - x1)) {
                 rsl = Math.abs(x2 - x1);
             }
         }
