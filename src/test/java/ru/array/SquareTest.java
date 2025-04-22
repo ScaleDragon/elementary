543feb59ab86f107820de41902e732919b6af8d6
Vyacheslav L. <Dwek18@mail.ru>
2025-04-22 16:34:55 +0300

    5.7 Массивы в тестах

--- src/test/java/ru/array/SquareTest.java
+++ src/test/java/ru/array/SquareTest.java
@@ -3,6 +3,8 @@ package ru.array;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class SquareTest {
 
     @Test
@@ -19,5 +21,21 @@ class SquareTest {
         int[] out = Square.calculate(size);
         int[] expected = {};
         Assertions.assertArrayEquals(expected, out);
+    }
+
+    @Test
+    void whenBound3Then014() {
+        int bound = 3;
+        int[] result = Square.calculate(bound);
+        int[] expected = new int[]{0, 1, 4};
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    void whenBound5Then014916() {
+        int bound = 5;
+        int[] result = Square.calculate(bound);
+        int[] expected = new int[]{0, 1, 4, 9, 16};
+        assertThat(result).containsExactly(expected);
     }
 }
