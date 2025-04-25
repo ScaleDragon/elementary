a7b1ec1c48ed6260b0f7bf1b4e607ae3d61a6cb5
Vyacheslav L. <Dwek18@mail.ru>
2025-04-25 11:21:18 +0300

    Задача 5.30 Выигрышные комбинации в сокобан

--- src/main/java/ru/array/MatrixCheck.java
+++ src/main/java/ru/array/MatrixCheck.java
@@ -30,14 +30,11 @@ public class MatrixCheck {
 
     public static boolean isWin(char[][] board) {
         for (int i = 0; i < board.length; i++) {
-            if (board[i][i] == 'X') {
-                if (monoHorizontal(board, i) | monoVertical(board, i)) {
-                    return true;
-                }
+            if (monoHorizontal(board, i) | monoVertical(board, i)) {
+                return true;
             }
         }
         return false;
     }
-
 }
 
