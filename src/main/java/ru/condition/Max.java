4c22fa2d0bfa96f3c2d262be9726f79cd780461f
Vyacheslav L. <Dwek18@mail.ru>
2025-05-02 01:28:03 +0300

    7.15 Перегрузить метод max для трех чисел

--- src/main/java/ru/condition/Max.java
+++ src/main/java/ru/condition/Max.java
@@ -4,4 +4,14 @@ public class Max {
     public static int max(int left, int right) {
         return left > right ? left : right;
     }
+
+    public static int max(int first, int second, int third) {
+        int maximum = max(first, second);
+        return maximum > third ? maximum : third;
+    }
+
+    public static int max(int first, int second, int third, int fourth) {
+        int maximum = max(first, second, third);
+        return maximum > fourth ? maximum : fourth;
+    }
 }
--- src/test/java/ru/condition/MaxTest.java
+++ src/test/java/ru/condition/MaxTest.java
@@ -41,4 +41,50 @@ class MaxTest {
         int expected = 3;
         assertThat(result).isEqualTo(expected);
     }
+
+    @Test
+    void whenMax2To6AndTo1Then6() {
+        int first = 2;
+        int second = 6;
+        int third = 1;
+        int result = Max.max(first, second, third);
+        int expected = 6;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenMax2To4AndTo7Then7() {
+        int first = 2;
+        int second = 4;
+        int third = 7;
+        int result = Max.max(first, second, third);
+        int expected = 7;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenMax10To4AndTo7AndTo10Then10() {
+        int first = 10;
+        int second = 4;
+        int third = 7;
+        int fourth = 10;
+        int result = Max.max(first, second, third, fourth);
+        int expected = 10;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenMax2To4AndTo7AndTo10Then10() {
+        int first = 2;
+        int second = 4;
+        int third = 7;
+        int fourth = 10;
+        int result = Max.max(first, second, third, fourth);
+        int expected = 10;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void testMax() {
+    }
 }
